7b356bc5e190bca43d0ca97029007de32c6dc884
Rohini Nair <rohiniacchu@gmail.com>
2021-04-21 13:39:00 +0530

    Return cordinates [x,y,z] for values in range x,y,z such that x+y+z!=n

---
+++ List_comprehension.py
@@ -0,0 +1,6 @@
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+z = int(input("Enter third number: "))
+n = int(input("Enter n value: "))
+result = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i+j+k)!=n]
+print(result)
