2af299e28b3b67999e53ff89740d7cd03e820651
Rohini Nair <rohiniacchu@gmail.com>
2021-04-23 13:16:40 +0530

    Abbreviations

--- ifelif_2.py
+++ ifelif_2.py
@@ -3,7 +3,7 @@ If the user enters "lol", print "laughing out loud".
 If the user enters "rofl", print "rolling on the floor laughing".
 If the user enters "lmk", print "let me know".
 If the user enters "smh", print "shaking my head"."""
-n=input("Enter the abbreviation: ")
+n=input("Enter the abbreviation: ").lower()
 if n=='lol':
     print("laughing out loud")
 elif n=='rofl':
