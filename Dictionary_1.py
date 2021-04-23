239607a588cad6d2b7accf97605c840873ef4af3
Rohini Nair <rohiniacchu@gmail.com>
2021-04-23 11:13:19 +0530

    Merge dict

---
+++ Dictionary_1.py
@@ -0,0 +1,16 @@
+"""Write a Python script to merge two Python dictionaries"""
+a = {}
+b={}
+def Merge(dict1,dict2):
+    res={**dict1,**dict2}
+    return res
+n = int(input("Enter number of pairs in dictionary 1:  "))
+for i in range(n):
+    key,value=input("Enter the "+str(i)+" pair").split()
+    a[key]=value
+m = int(input("Enter number of pairs in dictionary 2:  "))
+for i in range(m):
+    key,value=input("Enter the "+str(i)+" pair").split()
+    b[key]=value
+c=Merge(a,b)
+print(c)
