bb0671de73375ab96acd4c836354ad41db801a91
Rohini Nair <rohiniacchu@gmail.com>
2021-04-23 18:44:34 +0530

    Input Type

--- ifelif_4.py
+++ ifelif_4.py
@@ -10,18 +10,9 @@ def get_type(input_data):
     if input_data=='0':
         return 'Zero(0)'
     try:
-        return (literal_eval(input_data))
+        return type(literal_eval(input_data))
     except (ValueError, SyntaxError):
         # A string, so return str
         return str
 n=input("Enter Value: ")
-c=get_type(n)
-if c is float:
-    print("Float number: True")
-else:
-    print("Float number: False")
-if c is not str or c is not complex:
-    print("Real number: True")
-else:
-    print("Real number: False")
-if c is str
+print(get_type(n))
