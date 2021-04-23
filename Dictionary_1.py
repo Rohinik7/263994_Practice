836d1da16b119793b6e3c8ae4ca9472b9e39e3fc
Rohini Nair <59047425+Rohinik7@users.noreply.github.com>
2021-04-23 11:37:04 +0530

    Update Dictionary_1.py

--- Dictionary_1.py
+++ Dictionary_1.py
@@ -6,7 +6,7 @@ def Merge(dict1,dict2):
     return res
 n = int(input("Enter number of pairs in dictionary 1:  "))
 for i in range(n):
-    key,value=input("Enter the "+str(i)+" pair").split()
+    key,value=input("Enter the "+str(i+1)+" pair").split()
     a[key]=value
 m = int(input("Enter number of pairs in dictionary 2:  "))
 for i in range(m):
