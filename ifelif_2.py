25340a97e79c73c7bc6533ee1eb79237e57c1205
Rohini Nair <rohiniacchu@gmail.com>
2021-04-23 13:14:26 +0530

    Next day

---
+++ ifelif_2.py
@@ -0,0 +1,14 @@
+"""Write a python program to check the user input abbreviation.
+If the user enters "lol", print "laughing out loud".
+If the user enters "rofl", print "rolling on the floor laughing".
+If the user enters "lmk", print "let me know".
+If the user enters "smh", print "shaking my head"."""
+n=input("Enter the abbreviation: ")
+if n=='lol':
+    print("laughing out loud")
+elif n=='rofl':
+    print("rolling on the floor laughing")
+elif n=='lmk':
+    print("let me know")
+elif n=='smh':
+    print("shaking my head")
